3adce632a8807bf669bf483c7f289a0394c2e0e8
Ubuntu <hlfabric@hl-fabric-test.jigc532ivi5u3hgyh2ltdsknlh.tx.internal.cloudapp.net>
2023-02-17 10:35:33 +0000

    Added Code

---
+++ integerkey/application-gateway-go/integerkeyApp.go
@@ -0,0 +1,249 @@
+package main
+
+import (
+	"bytes"
+	"crypto/x509"
+	"encoding/json"
+	"fmt"
+	"os"
+	"path"
+	"time"
+	"strconv"
+
+	"github.com/hyperledger/fabric-gateway/pkg/client"
+	"github.com/hyperledger/fabric-gateway/pkg/identity"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+
+
+
+//	"net/http"
+//	"github.com/gin-gonic/gin"
+
+
+)
+
+const (
+	mspID        = "Org1MSP"
+	cryptoPath   = "/home/hlfabric/fabric-samples/test-network/organizations/peerOrganizations/org1.example.com"
+	certPath     = cryptoPath + "/users/User1@org1.example.com/msp/signcerts/cert.pem"
+	keyPath      = cryptoPath + "/users/User1@org1.example.com/msp/keystore/"
+	tlsCertPath  = cryptoPath + "/peers/peer0.org1.example.com/tls/ca.crt"
+	peerEndpoint = "localhost:7051"
+	gatewayPeer  = "peer0.org1.example.com"
+)
+
+
+
+type message_confirm struct{
+
+	status string `json:"status"`
+
+}
+
+
+
+
+
+func main() {
+	// gRPC client conn- shared by all gateway connections to this endpoint
+	clientConnection := newGrpcConnection()
+	defer clientConnection.Close()
+
+	//creating client identity, signing implementation
+	id := newIdentity()
+	sign := newSign()
+
+	gw, err := client.Connect(
+		id,
+		client.WithSign(sign),
+		client.WithClientConnection(clientConnection),
+		// Default timeouts for different gRPC calls
+		client.WithEvaluateTimeout(5*time.Second),
+		client.WithEndorseTimeout(15*time.Second),
+		client.WithSubmitTimeout(5*time.Second),
+		client.WithCommitStatusTimeout(1*time.Minute),
+	)
+
+	if err != nil {
+		panic(err)
+	}
+	defer gw.Close()
+	chaincodeName := "integerKey"
+	channelName := "tallychannel"
+	if cname := os.Getenv("CHANNEL_NAME"); cname != "" {
+		channelName = cname
+	}
+
+	network := gw.GetNetwork(channelName)
+	contract := network.GetContract(chaincodeName)
+
+
+	
+
+
+	createAsset(contract, "key1")
+	increaseValue(contract, "key1", 5) // we want to increase the value of the asset by 5
+	decreaseValue(contract, "key1", 2)
+	readAsset(contract, "key1")
+	readAsset(contract, "key2")
+	fmt.Print("TESTING DONE")
+
+
+
+
+//	router := gin.Default()
+//	router.POST("/createAsset", createAsset(contract))
+
+//	router.GET("/albums/:name", readAsset)
+
+//	router.Run("localhost:8080")
+
+
+}
+
+
+func newGrpcConnection() *grpc.ClientConn {
+	certificate, err := loadCertificate(tlsCertPath)
+	if err != nil {
+		panic(err)
+	}
+
+	certPool := x509.NewCertPool()
+	certPool.AddCert(certificate)
+	transportCredentials := credentials.NewClientTLSFromCert(certPool, gatewayPeer)
+
+	connection, err := grpc.Dial(peerEndpoint, grpc.WithTransportCredentials(transportCredentials))
+	if err != nil {
+		panic(fmt.Errorf("failed to create gRPC connection: %w", err))
+	}
+
+	return connection
+}
+
+// newIdentity creates a client identity for this Gateway connection using an X.509 certificate.
+func newIdentity() *identity.X509Identity {
+	certificate, err := loadCertificate(certPath)
+	if err != nil {
+		panic(err)
+	}
+
+	id, err := identity.NewX509Identity(mspID, certificate)
+	if err != nil {
+		panic(err)
+	}
+
+	return id
+}
+
+func loadCertificate(filename string) (*x509.Certificate, error) {
+	certificatePEM, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read certificate file: %w", err)
+	}
+	return identity.CertificateFromPEM(certificatePEM)
+}
+
+// newSign creates a function that generates a digital signature from a message digest using a private key.
+func newSign() identity.Sign {
+	files, err := os.ReadDir(keyPath)
+	if err != nil {
+		panic(fmt.Errorf("failed to read private key directory: %w", err))
+	}
+	privateKeyPEM, err := os.ReadFile(path.Join(keyPath, files[0].Name()))
+
+	if err != nil {
+		panic(fmt.Errorf("failed to read private key file: %w", err))
+	}
+
+	privateKey, err := identity.PrivateKeyFromPEM(privateKeyPEM)
+	if err != nil {
+		panic(err)
+	}
+
+	sign, err := identity.NewPrivateKeySign(privateKey)
+	if err != nil {
+		panic(err)
+	}
+
+	return sign
+}
+
+func createAsset(contract *client.Contract, name string) {
+	fmt.Printf("\n--> Submit Transaction: CreateAsset, creates new asset with name and default value set to 0 \n")
+
+//	c.BindJSON(&name)
+
+	_, err := contract.SubmitTransaction("CreateAsset", name)
+	if err != nil {
+
+
+	//	transaction_confirm.status="error in submitting create asset transaction"
+//		c.IndentedJSON(http.StatusCreated, transaction_confirm)
+
+		panic(fmt.Errorf("failed to submit transaction: %w", err))
+
+
+	}
+
+	fmt.Printf("*** Transaction committed successfully\n")
+
+
+
+
+
+//	transaction_confirm.status="asset created"
+//	c.IndentedJSON(http.StatusCreated, transaction_confirm)
+
+
+
+}
+
+func increaseValue(contract *client.Contract, name string, incVal uint) {
+	fmt.Printf("\n--> Submit Transaction: Increase Asset Value (by %v) \n", incVal)
+
+	_, err := contract.SubmitTransaction("IncreaseAsset", name, strconv.FormatUint(uint64(incVal),10))
+	if err != nil {
+		panic(fmt.Errorf("failed to submit transaction: %w", err))
+	}
+
+	fmt.Printf("*** Transaction committed successfully\n")
+}
+func decreaseValue(contract *client.Contract, name string, decVal uint) {
+	fmt.Printf("\n--> Submit Transaction: Increase Asset Value (by %v) \n", decVal)
+
+	_, err := contract.SubmitTransaction("IncreaseAsset", name, strconv.FormatUint(uint64(decVal),10))
+	if err != nil {
+		panic(fmt.Errorf("failed to submit transaction: %w", err))
+	}
+
+	fmt.Printf("*** Transaction committed successfully\n")
+}
+
+func readAsset(contract *client.Contract, name string) {
+	fmt.Printf("\n--> Evaluate Transaction: ReadAsset, function returns asset attributes\n")
+
+//	name := c.Param("name")
+	
+	evaluateResult, err := contract.EvaluateTransaction("ReadAsset", name)
+	if err != nil {
+		 panic(fmt.Errorf("failed to evaluate transaction: %w", err))
+
+
+	//	c.IndentedJSON(http.StatusNotImplemented, gin.H{"message": "failed to evaluate transaction"})
+
+	}
+	result := formatJSON(evaluateResult)
+
+//	c.IndentedJSON(http.StatusOK, result)
+
+	 fmt.Printf("*** Result:%s\n", result)
+}
+
+func formatJSON(data []byte) string {
+	var prettyJSON bytes.Buffer
+	if err := json.Indent(&prettyJSON, data, "", "  "); err != nil {
+		panic(fmt.Errorf("failed to parse JSON: %w", err))
+	}
+	return prettyJSON.String()
+}
---
+++ integerkey/chaincode-go/chaincode/smartcontract.go
@@ -0,0 +1,113 @@
+package chaincode
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/hyperledger/fabric-contract-api-go/contractapi"
+)
+
+type SmartContract struct {
+	contractapi.Contract
+}
+
+type Asset struct {
+	Name  string `json:"Name"`
+	Value uint   `json:"Value"`
+}
+
+func (s *SmartContract) AssetExists(ctx contractapi.TransactionContextInterface, Name string) (bool, error) {
+	assetJSON, err := ctx.GetStub().GetState(Name)
+	if err != nil {
+		return false, fmt.Errorf("failed to read from world state: %v", err)
+	}
+
+	return assetJSON != nil, nil
+}
+func (s *SmartContract) CreateAsset(ctx contractapi.TransactionContextInterface, Name string) error {
+	exists, err := s.AssetExists(ctx, Name)
+	if err != nil {
+		return err
+	}
+	if exists {
+		return fmt.Errorf("the asset %s already exists", Name)
+	}
+
+	asset := Asset{
+		Name:  Name,
+		Value: 0,
+	}
+	assetJSON, err := json.Marshal(asset)
+	if err != nil {
+		return err
+	}
+
+	return ctx.GetStub().PutState(Name, assetJSON)
+}
+
+// ReadAsset returns the asset stored in the world state with given Name.
+func (s *SmartContract) ReadAsset(ctx contractapi.TransactionContextInterface, Name string) (*Asset, error) {
+	assetJSON, err := ctx.GetStub().GetState(Name)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read from world state: %v", err)
+	}
+	if assetJSON == nil {
+		return nil, fmt.Errorf("the asset %s does not exist", Name)
+	}
+
+	var asset Asset
+	err = json.Unmarshal(assetJSON, &asset)
+	if err != nil {
+		return nil, err
+	}
+
+	return &asset, nil
+}
+func (s *SmartContract) IncreaseAsset(ctx contractapi.TransactionContextInterface, Name string, incrementValue uint) error {
+	// exists, err := s.AssetExists(ctx, Name)
+
+	asset_read, err := s.ReadAsset(ctx, Name)
+	if err != nil {
+		return err
+	}
+	// use GetState()
+	newValue := uint(asset_read.Value) + incrementValue
+
+	if newValue > 20 {
+		return fmt.Errorf("You cannot have a value more than 20.")
+	}
+
+	// overwriting original asset with new value
+	asset := Asset{
+		Name:  Name,
+		Value: newValue,
+	}
+	assetJSON, err := json.Marshal(asset)
+	if err != nil {
+		return err
+	}
+
+	return ctx.GetStub().PutState(Name, assetJSON)
+}
+
+func (s *SmartContract) DecreaseAsset(ctx contractapi.TransactionContextInterface, Name string, decrementValue uint) error {
+	asset_read, err := s.ReadAsset(ctx, Name)
+	if err != nil {
+		return err
+	}
+	newValue := uint(asset_read.Value) - decrementValue
+
+	// overwriting original asset with new value
+	asset := Asset{
+		Name:  Name,
+		Value: newValue,
+	}
+	assetJSON, err := json.Marshal(asset)
+	if err != nil {
+		return err
+	}
+
+	return ctx.GetStub().PutState(Name, assetJSON)
+}
+
+
