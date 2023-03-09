cbdc6485e030e6abe73c3e785f94905ed76f25e0
harithamadana <harithamadana@gmail.com>
2023-03-09 23:15:51 +0530

    trying getAllAssets

--- integerkey/react-integerkey-app/src/App.js
+++ integerkey/react-integerkey-app/src/App.js
@@ -1,12 +1,29 @@
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import IntegerKeyList from './components/IntegerKeyList';
+import { FaSyncAlt } from 'react-icons/fa';
 
 function App() {
-  return (
-    <div className="integerkey-app">
-      <IntegerKeyList/>
-    </div>
-  );
+        const [assets, setAssets] = useState([]);
+
+        const handleRefresh = async () => {
+          const response = await fetch('http://20.219.112.54:8080/integerKey/getAllAssets');
+          const data = await response.json();
+          setAssets(data);
+        };
+
+        useEffect(() => {
+          handleRefresh();
+        }, []);
+
+        return (
+          <div className="integerkey-app">
+            <button onClick={handleRefresh} className='refresh-button'>
+              <FaSyncAlt />
+            </button>
+            <IntegerKeyList assets={assets}/>
+          </div>
+        );
 }
 
 export default App;
