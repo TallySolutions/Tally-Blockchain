2345ef4c28f0ce780d311465c480be70937d0567
Tuhin Sengupta <tuhin.sengupts@tallysolutions.com>
2023-03-11 10:34:08 +0000

    Some more refactoring

--- Setup-Network/14_DeployChaincode.sh
+++ Setup-Network/14_DeployChaincode.sh
@@ -105,14 +105,14 @@ function checkPrereqs() {
 
 
 packageChaincode() {
-  set -x
+  
   /bin/rm -rf ${CC_PKG_PATH}
   /bin/mkdir -p ${CC_PKG_PATH}
   
   peer lifecycle chaincode package ${CC_PKG_PATH}/${CC_NAME}.tar.gz --path ${CC_SRC_PATH} --lang ${CC_RUNTIME_LANGUAGE} --label ${CC_NAME}_${CC_VERSION} >&${CC_PKG_PATH}/log.txt
   res=$?
   PACKAGE_ID=$(peer lifecycle chaincode calculatepackageid ${CC_PKG_PATH}/${CC_NAME}.tar.gz)
-  { set +x; } 2>/dev/null
+
   cat ${CC_PKG_PATH}/log.txt
   verifyResult $res "Chaincode packaging has failed"
   successln "Chaincode is packaged"
@@ -121,13 +121,12 @@ packageChaincode() {
 # installChaincode PEER ORG
 function installChaincode() {
 
-  set -x
+  
   peer lifecycle chaincode queryinstalled --output json | jq -r 'try (.installed_chaincodes[].package_id)' | grep ^${PACKAGE_ID}$ >&${CC_PKG_PATH}/log.txt
   if test $? -ne 0; then
     peer lifecycle chaincode install ${CC_PKG_PATH}/${CC_NAME}.tar.gz >&${CC_PKG_PATH}/log.txt
     res=$?
   fi
-  { set +x; } 2>/dev/null
   cat ${CC_PKG_PATH}/log.txt
   verifyResult $res "Chaincode installation on ${PEER_HOST} has failed"
   successln "Chaincode is installed on ${PEER_HOST}"
@@ -135,10 +134,9 @@ function installChaincode() {
 
 # queryInstalled PEER ORG
 function queryInstalled() {
-  set -x
+  
   peer lifecycle chaincode queryinstalled --output json | jq -r 'try (.installed_chaincodes[].package_id)' | grep ^${PACKAGE_ID}$ >&${CC_PKG_PATH}/log.txt
   res=$?
-  { set +x; } 2>/dev/null
   cat ${CC_PKG_PATH}/log.txt
   verifyResult $res "Query installed on ${PEER_HOST} has failed"
   successln "Query installed successful on ${PEER_HOST} on channel"
@@ -146,10 +144,9 @@ function queryInstalled() {
 
 # approveForMyOrg VERSION PEER ORG
 function approveForTally() {
-  set -x
+  
   peer lifecycle chaincode approveformyorg -o ${ORDERER_HOST}.${DOMAIN}:${ORDERER_PORT} --tls --cafile "${ORDERER_HOME}/msp/tlscacerts/tlsca.${DOMAIN}-cert.pem" --channelID ${CHANNEL_ID} --name ${CC_NAME} --version ${CC_VERSION} --package-id ${PACKAGE_ID} --sequence ${CC_SEQUENCE} ${INIT_REQUIRED} ${CC_END_POLICY} ${CC_COLL_CONFIG} >&${CC_PKG_PATH}/log.txt
   res=$?
-  { set +x; } 2>/dev/null
   cat ${CC_PKG_PATH}/log.txt
   verifyResult $res "Chaincode definition approved on ${PEER_HOST} on channel '$CHANNEL_ID' failed"
   successln "Chaincode definition approved on ${PEER_HOST} on channel '$CHANNEL_ID'"
@@ -165,10 +162,9 @@ function checkCommitReadiness()
   while [ $rc -ne 0 -a $COUNTER -lt $MAX_RETRY ]; do
     sleep $DELAY
     infoln "Attempting to check the commit readiness of the chaincode definition on $PEER_HOST, Retry after $DELAY seconds."
-    set -x
+    
     peer lifecycle chaincode checkcommitreadiness --channelID $CHANNEL_ID --name ${CC_NAME} --version ${CC_VERSION} --sequence ${CC_SEQUENCE} --output json >&${CC_PKG_PATH}/log.txt
     res=$?
-    { set +x; } 2>/dev/null
     let rc=0
     for var in "$@"; do
       grep "$var" ${CC_PKG_PATH}/log.txt &>/dev/null || let rc=1
@@ -188,10 +184,9 @@ function commitChaincodeDefinition()
   # while 'peer chaincode' command can get the orderer endpoint from the
   # peer (if join was successful), let's supply it directly as we know
   # it using the "-o" option
-  set -x
+  
   peer lifecycle chaincode commit -o ${ORDERER_HOST}.${DOMAIN}:${ORDERER_PORT} --tls --cafile "${ORDERER_HOME}/msp/tlscacerts/tlsca.${DOMAIN}-cert.pem" --channelID $CHANNEL_ID --name ${CC_NAME} --peerAddresses ${PEER_HOST}.${DOMAIN}:${PEER_PORT} --version ${CC_VERSION} --sequence ${CC_SEQUENCE} --tlsRootCertFiles ${PEER_HOME}/peers/${PEER_HOST}/tls/ca.crt >&${CC_PKG_PATH}/log.txt
   res=$?
-  { set +x; } 2>/dev/null
   cat ${CC_PKG_PATH}/log.txt
   verifyResult $res "Chaincode definition commit failed on $PEER_HOST on channel '$CHANNEL_ID' failed"
   successln "Chaincode definition committed on channel '$CHANNEL_ID'"
@@ -201,25 +196,24 @@ function commitChaincodeDefinition()
 function queryCommitted()
 {
   EXPECTED_RESULT="Version: ${CC_VERSION}, Sequence: ${CC_SEQUENCE}, Endorsement Plugin: escc, Validation Plugin: vscc"
-  infoln "Querying chaincode definition on peer0.org${ORG} on channel '$CHANNEL_ID'..."
+  infoln "Querying chaincode definition on ${PEER_HOST} on channel '$CHANNEL_ID'..."
   local rc=1
   local COUNTER=1
   # continue to poll
   # we either get a successful response, or reach MAX RETRY
   while [ $rc -ne 0 -a $COUNTER -lt $MAX_RETRY ]; do
     sleep $DELAY
-    echo "Attempting to Query committed status on ${PEER_HOST}, Retry after $DELAY seconds."
-    set -x
+    infoln "Attempting to Query committed status on ${PEER_HOST}, Retry after $DELAY seconds."
+    
     peer lifecycle chaincode querycommitted --channelID $CHANNEL_ID --name ${CC_NAME} >&${CC_PKG_PATH}/log.txt
     res=$?
-    { set +x; } 2>/dev/null
     test $res -eq 0 && VALUE=$(cat $CC_PKG_PATH/log.txt | grep -o '^Version: '$CC_VERSION', Sequence: [0-9]*, Endorsement Plugin: escc, Validation Plugin: vscc')
     test "$VALUE" = "$EXPECTED_RESULT" && let rc=0
     COUNTER=$(expr $COUNTER + 1)
   done
   cat $CC_PKG_PATH/log.txt
   if test $rc -eq 0; then
-    successln "Query chaincode definition successful on ${PEER_HOST} on channel '$CHANNEL_NAME'"
+    successln "Query chaincode definition successful on ${PEER_HOST} on channel '$CHANNEL_ID'"
   else
     fatalln "After $MAX_RETRY attempts, Query chaincode definition result on ${PEER_HOST} is INVALID!"
   fi
@@ -230,23 +224,45 @@ function chaincodeInvokeInit() {
   # while 'peer chaincode' command can get the orderer endpoint from the
   # peer (if join was successful), let's supply it directly as we know
   # it using the "-o" option
-  set -x
+  
   fcn_call='{"function":"'${CC_INIT_FCN}'","Args":[]}'
   infoln "invoke fcn call:${fcn_call}"
   peer chaincode invoke -o ${ORDERER_HOST}.${DOMAIN}:${ORDERER_PORT} --tls --cafile "${ORDERER_HOME}/msp/tlscacerts/tlsca.${DOMAIN}-cert.pem" -C $CHANNEL_ID -n ${CC_NAME} --peerAddresses ${PEER_HOST}.${DOMAIN}:${PEER_PORT}  --isInit -c ${fcn_call} >&$CC_PKG_PATH/log.txt
   res=$?
-  { set +x; } 2>/dev/null
   cat $CC_PKG_PATH/log.txt
   verifyResult $res "Invoke execution on $PEER_HOST failed "
-  successln "Invoke transaction successful on $PEER_HOST on channel '$CHANNEL_NAME'"
+  successln "Invoke transaction successful on $PEER_HOST on channel '$CHANNEL_ID'"
 }
 
+function chaincodeQuery() {
+  infoln "Querying on ${PEER_HOST} on channel '$CHANNEL_ID'..."
+  local rc=1
+  local COUNTER=1
+  # continue to poll
+  # we either get a successful response, or reach MAX RETRY
+  while [ $rc -ne 0 -a $COUNTER -lt $MAX_RETRY ]; do
+    sleep $DELAY
+    infoln "Attempting to Query ${PEER_HOST}, Retry after $DELAY seconds."
+    
+    peer chaincode query -C $CHANNEL_ID -n ${CC_NAME} -c '{"Args":["org.hyperledger.fabric:GetMetadata"]}' >&${CC_PKG_PATH}/log.txt
+    res=$?
+    let rc=$res
+    COUNTER=$(expr $COUNTER + 1)
+  done
+  cat ${CC_PKG_PATH}/log.txt
+  if test $rc -eq 0; then
+    successln "Query successful on ${PEER_HOST} on channel '$CHANNEL_ID'"
+  else
+    fatalln "After $MAX_RETRY attempts, Query result on ${PEER_HOST} is INVALID!"
+  fi
+}
 function deployCC()
 {
   
-   #Use peer host 1
+   #Use peer and orderer node as 1
   
    setPeerNode 1
+   setOrdererNode 1
 
    CC_PKG_PATH=${TALLY_HOME}/admin_client/chaincode/${CC_NAME}
    
@@ -260,7 +276,7 @@ function deployCC()
    infoln "Packaging chaincode on ${PEER_HOST}"
    packageChaincode
    
-   ## Install chaincode on peer0.org1 and peer0.org2
+   ## Install chaincode on ${PEER_HOST}.org1 and ${PEER_HOST}.org2
    infoln "Installing chaincode on ${PEER_HOST}"
    installChaincode 
 
@@ -269,16 +285,20 @@ function deployCC()
    queryInstalled
 
    ## approve the definition 
+   infoln "Approving chaincode on ${ORDERER_HOST}"
    approveForTally
 
    ## check whether the chaincode definition is ready to be committed
    ## expect Tally to have approved 
+   infoln "Checking commit readyness on ${ORDERER_HOST}"
    checkCommitReadiness "\"Tally\": true" 
 
    ## now that we know for sure both orgs have approved, commit the definition
+   infoln "Committing chaincode on ${ORDERER_HOST}"
    commitChaincodeDefinition
 
    ## query on both orgs to see that the definition committed successfully
+   infoln "Querying commit for chaincode on ${PEER_HOST}"
    queryCommitted 
 
    ## Invoke the chaincode - this does require that the chaincode have the 'initLedger'
@@ -290,8 +310,11 @@ function deployCC()
      chaincodeInvokeInit
    fi
 
+   
    successln "Chaincode deployed."
 
 }
 
-deployCC 
+deployCC
+
+chaincodeQuery
