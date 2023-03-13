cbf5227347b5d6d5799bd16c2427e97de4ca42b9
Tuhin Sengupta <tuhin.sengupts@tallysolutions.com>
2023-03-13 10:32:16 +0000

    Add npm web server deploy script

--- Setup-Network/16_DeployPeerWeb.sh
+++ Setup-Network/16_DeployPeerWeb.sh
@@ -12,7 +12,7 @@ function packagePeerWebServer()
 {
    infoln "Packaging Peer Web Server ..."
    mkdir -p $HOME/builds
-   tar -czf $HOME/builds/integerkeyapp-react-web.tar.gz ../integerkey/integerkeyapp-react-web 
+   tar -czf $HOME/builds/integerkeyapp-react-web.tar.gz --exclude="node_modules" ../integerkey/integerkeyapp-react-web 2> /dev/null
    verifyResult $? "Packaging failed."
 }
 
@@ -20,27 +20,32 @@ function startPeerWebServer()
 {
    setPeerNode $1
 
-   PID=$(checkIfRemoteProcessRunning integerkey-rest-api $PEER_HOST_USER $PEER_HOST.$DOMAIN $PEER_HOST_KEY)
-   if [[ $PID -ne -1 ]]; then
-      warnln "Peer Rest API Service already running on peer node $1 [PID: $PID], skipping start."
-      return 
-   fi
-
-   infoln "Copying Peer Rest API Service binary ..."
-   scp -C -i $PEER_HOST_KEY $HOME/builds/integerkey-rest-api ${PEER_HOST_USER}@${PEER_HOST}.${DOMAIN}:${NETWORK_HOME}/${PEER_NODE_FOLDER}/peers/${PEER_HOST}/.
+   infoln "Copying Peer Web Server Package ..."
+   ssh -i $PEER_HOST_KEY ${PEER_HOST_USER}@${PEER_HOST}.${DOMAIN} "mkdir -p ${NETWORK_HOME}/${PEER_NODE_FOLDER}/peers/${PEER_HOST}/web"
+   scp -C -i $PEER_HOST_KEY $HOME/builds/integerkeyapp-react-web.tar.gz ${PEER_HOST_USER}@${PEER_HOST}.${DOMAIN}:${NETWORK_HOME}/${PEER_NODE_FOLDER}/peers/${PEER_HOST}/web/.
    verifyResult $? "Copy failed."
 
-   ssh -i $PEER_HOST_KEY $PEER_HOST_USER@$PEER_HOST.$DOMAIN "cd $PEER_HOME_REMOTE; nohup ./integerkey-rest-api > rest-api-log.txt 2>&1 &"
-   PID=$(checkIfRemoteProcessRunning integerkey-rest-api $PEER_HOST_USER $PEER_HOST.$DOMAIN $PEER_HOST_KEY)
-   if [[ $PID -ne -1 ]]; then
-      successln "Peer Rest API Service started at peer node $1 [PID: $PID], you can check server log at $PEER_HOME_REMOTE/rest-api-log.txt."
+   infoln "Extracting Peer Web Server Package ..."
+   ssh -i $PEER_HOST_KEY $PEER_HOST_USER@$PEER_HOST.$DOMAIN "cd $PEER_HOME_REMOTE/web; tar -xzf integerkeyapp-react-web.tar.gz"
+   verifyResult $? "Extraction failed."
+
+   PID=$(checkIfRemoteProcessRunning npm $PEER_HOST_USER $PEER_HOST.$DOMAIN $PEER_HOST_KEY)
+   if [[ $PID -eq -1 ]]; then
+      infoln "Starting Peer Web Service on peer node $1 ..."
+      ssh -i $PEER_HOST_KEY $PEER_HOST_USER@$PEER_HOST.$DOMAIN "cd $PEER_HOME_REMOTE/web/integerkey/integerkeyapp-react-web; npm list ract || npm install react > $PEER_HOME_REMOTE/web/log.txt 2>&1; npm list react-id-generator || npm install react-id-generator  >> $PEER_HOME_REMOTE/web/log.txt 2>&1; npm list react-icons || npm install react-icons >> $PEER_HOME_REMOTE/web/log.txt 2>&1; nohup npm start >> $PEER_HOME_REMOTE/web/log.txt 2>&1 &"
+      PID=$(checkIfRemoteProcessRunning npm $PEER_HOST_USER $PEER_HOST.$DOMAIN $PEER_HOST_KEY)
+      if [[ $PID -ne -1 ]]; then
+          successln "Peer Web Server started at peer node $1 [PID: $PID], you can check server log at $PEER_HOME_REMOTE/web/log.txt."
+      else
+          errorln "Unable to start peer Web Server."
+      fi
    else
-      errorln "Unable to start peer Rest API service."
+      successln "Peer Web content updated at peer node $1 - web server already running [PID: $PID], you can check server log at $PEER_HOME_REMOTE/web/log.txt."
    fi
 }
 
 packagePeerWebServer
 
-#startPeerWebServer 1
-#startPeerWebServer 2
-#startPeerWebServer 3
+startPeerWebServer 1
+startPeerWebServer 2
+startPeerWebServer 3
