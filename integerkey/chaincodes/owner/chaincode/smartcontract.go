e4a512ae0b8a39b1715af8656a54a0940646a905
Tuhin Sengupta <tuhin.sengupts@tallysolutions.com>
2023-03-17 10:19:02 +0000

    Prefix editing in integerkey chaincode

--- integerkey/chaincodes/owner/chaincode/smartcontract.go
+++ integerkey/chaincodes/owner/chaincode/smartcontract.go
@@ -31,7 +31,7 @@ const Prefix = "Owner: "
 
 func (s *SmartContract) IsOwnerActive(ctx contractapi.TransactionContextInterface, Name string) (bool,error){
 	// returns boolean for owner status
-	ownerJSON, err := ctx.GetStub().GetState( Prefix + Name)
+	ownerJSON, err := ctx.GetStub().GetState(Prefix + Name)
 	if err != nil {
 
 		return false, fmt.Errorf("failed to read from world state: %v", err)
@@ -40,10 +40,12 @@ func (s *SmartContract) IsOwnerActive(ctx contractapi.TransactionContextInterfac
 
 	var owner OwnerAsset
 	err := json.Unmarshal([]byte(ownerJSON), &owner)
-	if err != nil {
-		fmt.Errorf("failed conversion to JSON in checking active status")
-		return nil, err
-	}
+
+		if err != nil {
+			fmt.Errorf("failed conversion to JSON in checking active status")
+			return nil, err
+		}
+
 	if owner.isActive{
 
 		fmt.Printf("Owner exists returned : %t\n", owner.isActive)
@@ -55,6 +57,21 @@ func (s *SmartContract) IsOwnerActive(ctx contractapi.TransactionContextInterfac
 
 }
 
+func (s *SmartContract) MakeOwnerActive(ctx contractapi.TransactionContextInterface , Name string) error{
+
+	ownerJSON, err := ctx.GetStub().GetState( Prefix + Name)
+		if err != nil {
+			return fmt.Errorf("Failed to read from world state: %v", err)
+		}
+	var owner OwnerAsset
+	err := json.Unmarshal([]byte(ownerJSON), &owner)
+		if err != nil {
+			fmt.Errorf("Failed conversion to JSON in checking active status")
+			return err
+		}
+	owner.isActive = true
+	return nil
+}
 
 
 func (s *SmartContract) OwnerExistence(ctx contractapi.TransactionContextInterface, Name string) (bool, error) {
@@ -62,7 +79,7 @@ func (s *SmartContract) OwnerExistence(ctx contractapi.TransactionContextInterfa
 
 	if err != nil {
 
-		return false, fmt.Errorf("failed to read from world state: %v", err)
+		return false, fmt.Errorf("Failed to read from world state: %v", err)
 
 	}
 
@@ -79,26 +96,37 @@ func (s *SmartContract) RegisterOwner(ctx TransactionContextInterface, Name stri
 
 	// if owner exists already
 	
-	if ownerexists {
-
-		// now there are 2 possible scenarios- active, the other is inactive
-
-		owneractive, err := s.IsOwnerActive(ctx, Name)
-
+		if ownerexists {
+				// now there are 2 possible scenarios- active, the other is inactive
+				owneractive, err := s.IsOwnerActive(ctx, Name)
+				if err != nil {
+					return err
+				}
+				if owneractive{		// if owner is active i.e. existing and active, a statement is returned that the user is already registered
+						fmt.Printf("ERRROR : Owner is already registered!")
+						return fmt.Errorf("Owner is already registered")
+				} else{	
+						// if the owner is not active, they are made active
+						err := s.MakeOwnerActive(ctx, Name)
+						if err != nil {
+							return fmt.Errorf("error in changing owner's status")
+						}
+						fmt.Printf("Owner is active")
+						return nil
+				}
+		} 
+		
+		// OWNER DOES NOT EXIST- So, now we will create a new owner, and register them too
+		owner := Owner{
+			OwnerName: Name,
+			OwnerID: uuid.New().String(),
+			isActive: true
+		}
+		ownerJSON, err := json.Marshal(owner)
 		if err != nil {
 			return err
 		}
-
-		if owneractive{		// if owner is active
-				
-		}
-
-	} else{
-
-		//create owner
-
-
-	}
+		state_err := ctx.GetStub().PutState(Prefix + Name, assetJSON) // new state added
 
 
 }
