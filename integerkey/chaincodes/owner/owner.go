239c6abdac3e98f455cffa94fc24370f10d39fb9
Tuhin Sengupta <tuhin.sengupts@tallysolutions.com>
2023-03-20 04:56:34 +0000

    owner.go

---
+++ integerkey/chaincodes/owner/owner.go
@@ -0,0 +1,23 @@
+/*
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package main
+
+import (
+	"log"
+
+	"github.com/hyperledger/fabric-contract-api-go/contractapi"
+	"tallysolutions.com/integerKey/chaincodes/owner/chaincode"
+)
+
+func main() {
+	assetChaincode, err := contractapi.NewChaincode(&chaincode.SmartContract{})
+	if err != nil {
+		log.Panicf("Error creating owner chaincode: %v", err)
+	}
+
+	if err := assetChaincode.Start(); err != nil {
+		log.Panicf("Error starting owner chaincode: %v", err)
+	}
+}
