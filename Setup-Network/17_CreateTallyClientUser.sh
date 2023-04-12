4cb8bd5a07c2311cc6aa2e197f8f1ba0c0576899
Tuhin Sengupta <tuhin.sengupts@tallysolutions.com>
2023-04-12 08:01:34 +0000

    added approver and creator logic

--- Setup-Network/17_CreateTallyClientUser.sh
+++ Setup-Network/17_CreateTallyClientUser.sh
@@ -7,7 +7,7 @@ setCANode 1
 
 function printHelp()
 {
-  warnln "Usage: 17_CreateTallyClientUser.sh <User Id> <Is_Approver>"               # How do we get CA_HONE and TYPE values? Ans: line 31 are params to function, the input params in line 31= those from cli
+  warnln "Usage: 17_CreateTallyClientUser.sh <User Id> <Is_Approver> <Is_Creator>"               # How do we get CA_HONE and TYPE values? Ans: line 31 are params to function, the input params in line 31= those from cli
 }
 if [[ $# -lt 1 ]] ; then
   printHelp
@@ -22,14 +22,14 @@ function RegisterUser()
         APPROVER=$4
         CREATOR=$5
         export FABRIC_CA_CLIENT_HOME=${CA_HOME}/client
-        infoln "Registering user ${USER} for ${CA_HOME} [${TYPE}] \n Is approver?= ${APPROVER}"
-        fabric-ca-client register --id.name ${USER} --id.type ${TYPE} --id.affiliation tally --id.attrs 'approver='$APPROVER':ecert, creator:'$CREATOR'' --tls.certfiles "${CA_HOME}/ca-cert.pem"  # added attributes for id affiliations and other attrs
+        infoln "Registering user ${USER} for ${CA_HOME} [${TYPE}] \n Is approver?= ${APPROVER} \n Is creator?= ${CREATOR}"
+        fabric-ca-client register --id.name ${USER} --id.type ${TYPE} --id.affiliation tally --id.attrs 'approver='$APPROVER':ecert, creator:'$CREATOR':ecert' --tls.certfiles "${CA_HOME}/ca-cert.pem"  # added attributes for id affiliations and other attrs
         res=$?
         verifyResult $res "Unable to register user for ${CA_HOME} [${TYPE}]. (Are you trying to re-register a user or CA server is stopped?)"
 
 }
 
-RegisterUser ${TALLY_CA_HOME} $1 client $2
+RegisterUser ${TALLY_CA_HOME} $1 client $2 $3
 
 
 # creator and approver- categories for users
