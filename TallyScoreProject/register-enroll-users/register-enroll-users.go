438bbdfacad96750230c47db81edd63f078c4c12
Tuhin Sengupta <tuhin.sengupts@tallysolutions.com>
2023-05-15 11:50:28 +0000

    added code for enrolling user
    TODO: fix no o/p problem

--- TallyScoreProject/register-enroll-users/register-enroll-users.go
+++ TallyScoreProject/register-enroll-users/register-enroll-users.go
@@ -14,19 +14,23 @@ var tallyHome string
 var caServerHome string
 var tallyCAHome string
 var fabric_ca_client_home string
-var url string
+var urlend string
 
 const(
 		// add the relevant env vars(from setup network) as constants
 		// var- TALLY NETWORK HOME- add the env locations with this as the common base
 		networkHome= "fabric/tally-network"
 		tallyCAName= "tally"
+		ca_host="tbchlfdevca01"
+		domain="tally.tallysolutions.com"
+		tally_ca_port="7055"
+		urlstart= "https://"
 )
 
 
 func printUsage() {
 	panic("Format to register user:\n" +
-		"Register <user_id>" + "\n")
+		"go run . <user_id>" + "\n")
 }
 
 // func printUsage() {
@@ -41,52 +45,41 @@ func main(){
 	caServerHome= tallyHome + "/fabric-ca-servers"
 	tallyCAHome= caServerHome + "/" + tallyCAName
 	fabric_ca_client_home= tallyCAHome + "/client"
+	urlend= "@" + ca_host + "." + domain + ":" + tally_ca_port
 
 	if len(os.Args)<2{
 		printUsage()
 	}
 
-	operation:=os.Args[0]
+	operation:= os.Args[0]
 	if operation == "Register" && len(os.Args)<2{
 		printUsage()
 	}
 
-	if operation== "Register"{
+	// if operation == "Register"{
 		userId:=os.Args[1]
-		fmt.Printf("Initiating registration of user")
+		fmt.Printf("Initiating registration of user\n")
 		password, err := registerUser(userId)
 		if err!=nil{
-			fmt.Errorf("Error in the process of registration of user")
+			fmt.Errorf("Error in the process of registration of user\n")
 		}
+		fmt.Printf("Password: %s\n", password)
 
-		fmt.Printf("Initial stage of registration successful! Initiating enrollment of user now.")
-		// write code to enroll user
+		fmt.Printf("Initial stage of registration successful! Initiating enrollment of user now.\n")
+		// write code to call enrollUser() function
+		userMSP, err:= enrollUser(userId, password)
+		if err!= nil{
+			fmt.Errorf("Error in enrollment stage\n")
+		}
+		fmt.Printf("MSP path of User: %s \n", userMSP)
+		fmt.Printf("Registration of User successful!\n")
 
-	}
-	// if operation == "Enroll" && len(os.Args)<3{
-	// 	printUsage()
-	// }
-
-	// // userObj := User{
-	// // 	UserId: "user: "
-	// // 	Password: " "
-	// // }
-
-	// if operation == "Register"{
-	// 	fmt.Printf("Intiating registration of user...\n")
-	// 	userId:=os.Args[1]
-	// 	registerUser(userId)
-	// } else if operation == "Enroll"{
-	// 	fmt.Printf("Intiating enrollment of user...\n")
-	// 	userId:=os.Args[1]
-	// 	password:=os.Args[2]
-	// 	enrollUser(userId, password)
-	// }
-
+//	}
 
 }
 
 func registerUser(userId string) (string, error){   // this function should take in userid and print the password
+
 	cmdVariable := exec.Command("fabric-ca-client", "register", "--id.name", userId, "--id.type", "client", "--id.affiliation", "tally", "--tls.certfiles", fmt.Sprintf("%s/ca-cert.pem", tallyCAHome))
 	cmdVariable.Env = append(cmdVariable.Env, fmt.Sprintf("FABRIC_CA_CLIENT_HOME=%s", fabric_ca_client_home))
 	output, err := cmdVariable.Output()
@@ -99,8 +92,19 @@ func registerUser(userId string) (string, error){   // this function should take
 
 }
 
-func enrollUser(userId string, password string){  // this function should take in userid and password, then it should return/print the public+private key msp
-	// USE ID.SECRETS FOR PASSWORD- do not make this a constant in the prog
+func enrollUser(userId string, password string) (string, error) {  // this function should take in userid and password, then it should return/print the public+private key msp
+
+	// urlmid would be like-> <userId>:<password>
+	cmdVariable:= exec.Command("fabric-ca-client", "enroll", "-u", urlstart + userId + ":" + password + urlend)
+	cmdVariable.Env = append(cmdVariable.Env, fmt.Sprintf("FABRIC_CA_CLIENT_HOME=%s", fabric_ca_client_home))
+	err := cmdVariable.Run()
+	if err != nil {
+		return "", err
+	}
+	
+	mspPath := fmt.Sprintf("%s/msp", fabric_ca_client_home)
+
+	return mspPath, nil
 
 }
 
