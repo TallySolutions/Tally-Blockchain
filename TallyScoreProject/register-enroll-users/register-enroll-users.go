dee4d13043203b326347947e147119cb8c500295
Tuhin Sengupta <tuhin.sengupts@tallysolutions.com>
2023-05-22 07:02:38 +0000

    registration+enrollment of users done; along with basicfront end of website

--- TallyScoreProject/register-enroll-users/register-enroll-users.go
+++ TallyScoreProject/register-enroll-users/register-enroll-users.go
@@ -47,21 +47,13 @@ func main(){
 	fabric_ca_client_home= tallyCAHome + "/client"
 	urlend= "@" + ca_host + "." + domain + ":" + tally_ca_port
 
-	if len(os.Args)<2{
-		printUsage()
-	}
-
-	operation:= os.Args[0]
-	if operation == "Register" && len(os.Args)<2{
-		printUsage()
-	}
-
-	// if operation == "Register"{
-		userId:=os.Args[1]
-		fmt.Printf("Initiating registration of user\n")
+		userId:= os.Args[1]
+		fmt.Printf("Initiating registration of user %s\n", userId)
 		password, err := registerUser(userId)
 		if err!=nil{
+			fmt.Printf("Error in step 1\n")
 			fmt.Errorf("Error in the process of registration of user\n")
+			return
 		}
 		fmt.Printf("Password: %s\n", password)
 
@@ -70,24 +62,38 @@ func main(){
 		userMSP, err:= enrollUser(userId, password)
 		if err!= nil{
 			fmt.Errorf("Error in enrollment stage\n")
+			return
 		}
 		fmt.Printf("MSP path of User: %s \n", userMSP)
 		fmt.Printf("Registration of User successful!\n")
-
-//	}
 
 }
 
 func registerUser(userId string) (string, error){   // this function should take in userid and print the password
 
 	cmdVariable := exec.Command("fabric-ca-client", "register", "--id.name", userId, "--id.type", "client", "--id.affiliation", "tally", "--tls.certfiles", fmt.Sprintf("%s/ca-cert.pem", tallyCAHome))
+	// set max enrollments
 	cmdVariable.Env = append(cmdVariable.Env, fmt.Sprintf("FABRIC_CA_CLIENT_HOME=%s", fabric_ca_client_home))
-	output, err := cmdVariable.Output()
+
+	fmt.Printf("cmd Env: %s\n",cmdVariable.Env)
+
+	fmt.Printf("FABRIC CA CLIENT HOME PATH: %s \n", fabric_ca_client_home)
+
+
+	fmt.Printf("The command executed while registering:%s\n", cmdVariable.String())
+	fmt.Print("Value of tallyCAHome:", tallyCAHome, "\n")
+	fmt.Printf("Path to ca-certs:%s \n", fmt.Sprintf("%s/ca-cert.pem", tallyCAHome))
+
+
+	output, err := cmdVariable.CombinedOutput()
+
+	fmt.Printf("Value of cmdVariable's output string: %s\n", string(output) )
+
 	if err != nil {
 		return "",err
 	}
+	
 	password := getPassword(string(output)) // extract password from the cli's output
-	fmt.Printf("Password:%s", password)
 	return password,nil
 
 }
@@ -97,15 +103,15 @@ func enrollUser(userId string, password string) (string, error) {  // this funct
 	// urlmid would be like-> <userId>:<password>
 
 	mspPath := fmt.Sprintf("%s/users/%s", fabric_ca_client_home, userId) +"/msp"
-	cmdVariable:= exec.Command("fabric-ca-client", "enroll", "-u", urlstart + userId + ":" + password + urlend , "--csr.names", "C=IN,ST=Bengaluru,L=Bengaluru,O=Tally,OU=client", "-M", mspPath, "--tls.certfiles", fmt.Sprintf("%s/ca-cert.pem", tallyCAHome))
+	cmdVariable:= exec.Command("fabric-ca-client", "enroll", "-u", urlstart + userId + ":" + password + urlend , "--csr.names", "C=IN,ST=Karnataka,L=Bengaluru,O=Tally,OU=client", "-M", mspPath, "--tls.certfiles", fmt.Sprintf("%s/ca-cert.pem", tallyCAHome))
+	fmt.Printf("%s", cmdVariable.String())
 	cmdVariable.Env = append(cmdVariable.Env, fmt.Sprintf("FABRIC_CA_CLIENT_HOME=%s", fabric_ca_client_home))
 	err := cmdVariable.Run()
+	fmt.Printf("%v \n", err)
 	if err != nil {
 		return "", err
 	}
-	
-	
-
+	// return content of mspPath- with the signcert as a param, private key as a param, tls and ca-certs  then DELETE the mspPath folder
 	return mspPath, nil
 
 }
