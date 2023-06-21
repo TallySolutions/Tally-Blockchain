ab439f7506261c271ff51dff40412774c8948bc9
Tuhin Sengupta <tuhin.sengupta@tallysolutions.com>
2023-06-21 13:33:40 +0530

    instance id change for ord02

--- Setup-Network/stop-instanses.bat
+++ Setup-Network/stop-instanses.bat
@@ -1,1 +1,1 @@
-aws ec2 stop-instances --instance-ids i-0107eb0b8116b6393 i-01127dad19a1dd53d i-0fc34c095d281c2e9 i-0df52417f01fa2341 i-0d949bd21218a207c i-0bd92c6b472459c6a i-0109ee191f89d9eb4
+aws ec2 stop-instances --instance-ids i-0107eb0b8116b6393 i-01127dad19a1dd53d i-0109ee191f89d9eb4 i-0df52417f01fa2341 i-0d949bd21218a207c i-0bd92c6b472459c6a i-0109ee191f89d9eb4
