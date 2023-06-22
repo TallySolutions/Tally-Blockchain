2ac0e87fc41986225922c9568ad9f576f3238dd2
Tuhin Sengupta <tuhin.sengupta@tallysolutions.com>
2023-06-22 04:41:25 +0000

    Added SmartEVM

---
+++ SmartEVM/chaincode/voterContract/voterContract.go
@@ -0,0 +1,22 @@
+/*
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package main
+
+import (
+	"log"
+	"github.com/hyperledger/fabric-contract-api-go/contractapi"
+	"tallysolutions.com/SmartEVM/chaincode/voterContract/chaincode"
+)
+
+func main() {
+	assetChaincode, err := contractapi.NewChaincode(&chaincode.SmartContract{})
+	if err != nil {
+		log.Panicf("Error creating voterContract chaincode: %v", err)
+	}
+
+	if err := assetChaincode.Start(); err != nil {
+		log.Panicf("Error starting voterContract chaincode: %v", err)
+	}
+}
