27b0f9d431482ae5f2da96461fe9f9b27dd8bce8
Tuhin Sengupta <tuhin.sengupta@tallysolutions.com>
2023-06-23 08:41:17 +0000

    Added unit tests for Add

--- SmartEVM/chaincode/voterContract/chaincode/voterContractCC.go
+++ SmartEVM/chaincode/voterContract/chaincode/voterContractCC.go
@@ -1,107 +1,130 @@
 package chaincode
 
-import(
+import (
 	"encoding/json"
 	"fmt"
-	"strconv"
-	"log"
+	"time"
+
 	"github.com/hyperledger/fabric-contract-api-go/contractapi"
 )
 
-const Abstained="_Abstained_"
+const Abstained = "_Abstained_"
 
 type SmartContract struct {
-    contractapi.Contract
-    IsAnonymous bool
-    Initialized bool
-    Abstainable bool
-    SingleChoce bool
+	contractapi.Contract
+	IsAnonymous  bool
+	Initialized  bool
+	Abstainable  bool
+	SingleChoice bool
 }
 
+type Marshaler interface {
+	MarshalJSON() ([]byte, error)
+}
 
-// NOTE: Write the asset properties in CAMEL CASE- otherwise, chaincode will not get deployed 
+type DateTime time.Time
+
+func (t DateTime) MarshalJSON() ([]byte, error) {
+	//do your serializing here
+	stamp := fmt.Sprintf("\"%s\"", time.Time(t).Format("RFC3339"))
+	return []byte(stamp), nil
+}
+
+// NOTE: Write the asset properties in CAMEL CASE- otherwise, chaincode will not get deployed
 type Ballot struct {
-	CastedBy  string    `json:"CastedBy"`
-	Timestamp time.Time `json:"Timestamp"`
+	CastedBy  string   `json:"CastedBy"`
+	Timestamp DateTime `json:"Timestamp"`
 }
 type VotableOption struct {
 	VotableId string   `json:"VotableId"`
 	Ballots   []Ballot `json:"Ballots"`
 }
 
-//Init Ledger
-func (s *SmartContract) IntiLedger(ctx contractapi.TransactionContextInterface) {
+// Utility function
+func contains(s []string, e string) bool {
+	for _, a := range s {
+		if a == e {
+			return true
+		}
+	}
+	return false
+}
+
+// Init Ledger
+func (s *SmartContract) InitLedger(ctx contractapi.TransactionContextInterface) error {
 	//TODO: values must come from chaincode deployment args
 	s.Initialized = true
 	s.IsAnonymous = true
 	s.Abstainable = true
 	s.SingleChoice = true
-	if a.Abstainable {
-		err := s.addVotableOption(ctx, Abstained)
+	if s.Abstainable {
+		err := s.AddVotableOption(ctx, Abstained)
 		if err != nil {
 			s.Initialized = false
-			log.Fatal(err)
+			return err
 		}
 	}
+	return nil
 }
 
-//Is Votable Option Exists?
+// Is Votable Option Exists?
 func (s *SmartContract) isVotableOptionExists(ctx contractapi.TransactionContextInterface, votableId string) (bool, error) {
 	if s.Initialized != true {
-		return false,  fmt.Errorf("Ledger not inialized!")
+		return false, fmt.Errorf("Ledger not inialized!")
 	}
 	optionBytes, err := ctx.GetStub().GetState(votableId)
 	if err != nil {
-		return false, fmt.Errorf("failed to read asset %s from world state. %v", votableId, err)
+		return false, err
 	}
 
 	return optionBytes != nil, nil
 }
 
-// function to add Votable Option 
-func (s *SmartContract) addVotableOption(ctx contractapi.TransactionContextInterface, votableId string) error{
+// function to add Votable Option
+func (s *SmartContract) AddVotableOption(ctx contractapi.TransactionContextInterface, votableId string) error {
 
 	if s.Initialized != true {
-		return false,  fmt.Errorf("Ledger not inialized!")
+		return fmt.Errorf("Ledger not inialized!")
 	}
-    	fmt.Printf("Adding new votable option: %s\n", votableId)
+	fmt.Printf("Adding new votable option: %s\n", votableId)
 	//checking if option already added
-	optionExists,err:= s.isVotableOptionExists(ctx, votableId)
-	if err!=nil{
-		return fmt.Errorf("error in checking whether asset exists or not: %v", err)
+	optionExists, err := s.isVotableOptionExists(ctx, votableId)
+	if err != nil {
+		return err
 	}
 	if optionExists {
 		return fmt.Errorf("This votable option is already exist!")
 	}
 
 	// if the option does not exist
+	var ballots []Ballot
 	votableOption := VotableOption{
 		VotableId: votableId,
-		Ballots: []Ballot
+		Ballots:   ballots,
 	}
 	votableOptionJSON, err := json.Marshal(votableOption)
-    	if err != nil {
-        	return err
-    	}
+	if err != nil {
+		return err
+	}
 
-    	fmt.Printf("Creating new asset for this votable id in voting ledger: %s\n", votableId)
+	fmt.Printf("Creating new asset for this votable id in voting ledger: %s\n", votableId)
 	putStateErr := ctx.GetStub().PutState(votableId, votableOptionJSON) // new state added to the voting ledger
-    	return putStateErr
+	return putStateErr
 
-} 
+}
 
 // function to read vote option
-func (s *SmartContract) ReadOption(ctx contractapi.TransactionContextInterface, votableId string) (*VotableOption, error){
+func (s *SmartContract) ReadOption(ctx contractapi.TransactionContextInterface, votableId string) (*VotableOption, error) {
 	if s.Initialized != true {
-		return false,  fmt.Errorf("Ledger not inialized!")
+		return nil, fmt.Errorf("Ledger not inialized!")
 	}
 	votableOptionJSON, err := ctx.GetStub().GetState(votableId)
 	if err != nil {
 		return nil, fmt.Errorf("Failed to read from world state: %v", err)
 	}
 	if votableOptionJSON == nil {
-    		return nil, fmt.Errorf("The votable id %s is not present.", votableId)
-    	}
+		return nil, fmt.Errorf("The votable id %s is not present.", votableId)
+	}
 
 	var votableOption VotableOption
 	err = json.Unmarshal(votableOptionJSON, &votableOption)
@@ -113,78 +136,87 @@ func (s *SmartContract) ReadOption(ctx contractapi.TransactionContextInterface, 
 }
 
 // function to cast vote
-func (s *SmartContract) castVote(ctx contractapi.TransactionContextInterface,userId string, votableIds []string) error {
+func (s *SmartContract) CastVote(ctx contractapi.TransactionContextInterface, userId string, votableIds []string) error {
 	if s.Initialized != true {
-		return false,  fmt.Errorf("Ledger not inialized!")
+		return fmt.Errorf("Ledger not inialized!")
 	}
-	
+
 	if len(votableIds) == 0 {
-		if s.IsAbstainable {
-			return castVote(str, {Abstained})
+		if s.Abstainable {
+			return s.CastVote(ctx, userId, []string{Abstained})
 		}
-		return fmt.Error("Number of votes to be casted can not be zero.")
+		return fmt.Errorf("Number of votes to be casted can not be zero.")
 	}
-	if ( len(votableIds) > 1 && s.SingleChoice ) {
-		return fmt.Error("Number of votes can not be more than one, in case of single choice voting.")
+	if len(votableIds) > 1 && s.SingleChoice {
+		return fmt.Errorf("Number of votes can not be more than one, in case of single choice voting.")
 	}
 	//Get all current voting options
 	fmt.Println("Fetching all possible votable options ...")
 	options, err := s.GetAllOptions(ctx)
 	if err != nil {
-		fmt.Println("Could not get all voting options : %v", err)
+		fmt.Println("Could not get all voting options : ", err)
 		return err
 	}
-	
-	var updatedOptions []VotableOptions
 
-	for i, votableId := range options {
-		fmt.Println(i, "->", votableId) 
-		votableOption, err := s.ReadOption(ctx, votableId) // asset is read
-		if err != nil {
-			fmt.Println("Could not retrieve option %s : %v", votableId, err)
-			continue
-		}
+	var updatedOptions []VotableOption
 
-		for j, ballot := votableOption.Ballots {
+	for i, votableOption := range options {
+		fmt.Println(i, "->", votableOption.VotableId)
+
+		for j, ballot := range votableOption.Ballots {
 			fmt.Println("   Ballot ", j, "->", ballot.Timestamp, ";", ballot.CastedBy)
 			if ballot.CastedBy == userId {
-				return fmt.Error("User %s already casted the vote!", userId)
+				return fmt.Errorf("User %s already casted the vote!", userId)
 			}
 		}
-		if contains(votableIds, votablId) {
-			append(votableId.Ballots, Ballot {CastedBy: s.IsAnonymous?"Anonymous":userId, Timestamp: time.Now})
-			append(updatedOptions, votableId)
+		if contains(votableIds, votableOption.VotableId) {
+			castedBy := userId
+			if s.IsAnonymous {
+				castedBy = "Anonymous"
+			}
+			ballot := Ballot{CastedBy: castedBy, Timestamp: DateTime(time.Now())}
+			votableOption.Ballots = append(votableOption.Ballots, ballot)
+			updatedOptions = append(updatedOptions, *votableOption)
 		}
 	}
 	for i, updated := range updatedOptions {
-		fmt.Println("Registering vote for %s : %s", userId, updated.VotableId)
+		fmt.Printf("(%d) Registering vote for %s : %s\n", i, userId, updated.VotableId)
 		updatedJSON, err := json.Marshal(updated)
 		if err != nil {
 			return err
 		}
 		updatestate_err := ctx.GetStub().PutState(updated.VotableId, updatedJSON)
+		if updatestate_err != nil {
+			return updatestate_err
+		}
 	}
 	return nil
-   }
 }
+
 // GetAllAssets returns all voting options found in world state
-func (s *SmartContract) GetAllOptions(ctx contractapi.TransactionContextInterface) ([]*VotingOption, error) {
+func (s *SmartContract) GetAllOptions(ctx contractapi.TransactionContextInterface) ([]*VotableOption, error) {
 	// range query with empty string for startKey and endKey does an
 	// open-ended query of all assets in the chaincode namespace.
 	resultsIterator, err := ctx.GetStub().GetStateByRange("", "")
+
 	if err != nil {
 		return nil, err
 	}
+
+	if resultsIterator == nil {
+		return nil, fmt.Errorf("No result iterator found!")
+	}
+
 	defer resultsIterator.Close()
 
-	var assets []*VotingOption
+	var assets []*VotableOption
 	for resultsIterator.HasNext() {
 		queryResponse, err := resultsIterator.Next()
 		if err != nil {
 			return nil, err
 		}
-
-		var asset VotingOption
+		fmt.Println(queryResponse.Value)
+		var asset VotableOption
 		err = json.Unmarshal(queryResponse.Value, &asset)
 		if err != nil {
 			return nil, err
