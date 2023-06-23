83e8e29efc89e9283bf0dbe725c56a48d93721b6
Tuhin Sengupta <tuhin.sengupta@tallysolutions.com>
2023-06-23 06:31:01 +0000

    Implemented Voter Contract CC

--- SmartEVM/chaincode/voterContract/chaincode/voterContractCC.go
+++ SmartEVM/chaincode/voterContract/chaincode/voterContractCC.go
@@ -4,166 +4,193 @@ import(
 	"encoding/json"
 	"fmt"
 	"strconv"
+	"log"
 	"github.com/hyperledger/fabric-contract-api-go/contractapi"
 )
 
+const Abstained="_Abstained_"
+
 type SmartContract struct {
     contractapi.Contract
+    IsAnonymous bool
+    Initialized bool
+    Abstainable bool
+    SingleChoce bool
 }
 
 
 // NOTE: Write the asset properties in CAMEL CASE- otherwise, chaincode will not get deployed 
-type TallyScoreAsset struct {
-    LicenseId string `json:"LicenseId"`
-	Score uint `json:"Score"`
+type Ballot struct {
+	CastedBy  string    `json:"CastedBy"`
+	Timestamp time.Time `json:"Timestamp"`
+}
+type VotableOption struct {
+	VotableId string   `json:"VotableId"`
+	Ballots   []Ballot `json:"Ballots"`
 }
 
-//function to check whether the company has already been registered
-func (s *SmartContract) companyAssetExists(ctx contractapi.TransactionContextInterface, LicenseId string) (bool, error) {
-
-    companyAssetJSON, err := ctx.GetStub().GetState(LicenseId)
-    if err != nil {
-    	return false, fmt.Errorf("failed to read from world state: %v", err)
-    }
-
-    return companyAssetJSON != nil, nil
+//Init Ledger
+func (s *SmartContract) IntiLedger(ctx contractapi.TransactionContextInterface) {
+	//TODO: values must come from chaincode deployment args
+	s.Initialized = true
+	s.IsAnonymous = true
+	s.Abstainable = true
+	s.SingleChoice = true
+	if a.Abstainable {
+		err := s.addVotableOption(ctx, Abstained)
+		if err != nil {
+			s.Initialized = false
+			log.Fatal(err)
+		}
+	}
 }
 
-// function to register company and initialize it's Score to 500 
-func (s *SmartContract) RegisterCompany(ctx contractapi.TransactionContextInterface, LicenseId string) error{
+//Is Votable Option Exists?
+func (s *SmartContract) isVotableOptionExists(ctx contractapi.TransactionContextInterface, votableId string) (bool, error) {
+	if s.Initialized != true {
+		return false,  fmt.Errorf("Ledger not inialized!")
+	}
+	optionBytes, err := ctx.GetStub().GetState(votableId)
+	if err != nil {
+		return false, fmt.Errorf("failed to read asset %s from world state. %v", votableId, err)
+	}
 
-	// ------------------------- use client.Context to retrieve the PAN value of the business ------------------------
+	return optionBytes != nil, nil
+}
 
-	//checking if licenseID is valid
-	companyAssetExists,err:= s.companyAssetExists(ctx, LicenseId)
+// function to add Votable Option 
+func (s *SmartContract) addVotableOption(ctx contractapi.TransactionContextInterface, votableId string) error{
+
+	if s.Initialized != true {
+		return false,  fmt.Errorf("Ledger not inialized!")
+	}
+    	fmt.Printf("Adding new votable option: %s\n", votableId)
+	//checking if option already added
+	optionExists,err:= s.isVotableOptionExists(ctx, votableId)
 	if err!=nil{
-		return fmt.Errorf("error in checking whether asset exists: %v", err)
+		return fmt.Errorf("error in checking whether asset exists or not: %v", err)
 	}
-	if companyAssetExists {
-		return fmt.Errorf("This company already exists!")
+	if optionExists {
+		return fmt.Errorf("This votable option is already exist!")
 	}
 
-	// if the company is unregistered
-	companyScoreAsset := TallyScoreAsset{
-		LicenseId: LicenseId,
-		Score: 500,
+	// if the option does not exist
+	votableOption := VotableOption{
+		VotableId: votableId,
+		Ballots: []Ballot
 	}
-	companyScoreAssetJSON, err := json.Marshal(companyScoreAsset)
-    if err != nil {
-        return err
-    }
+	votableOptionJSON, err := json.Marshal(votableOption)
+    	if err != nil {
+        	return err
+    	}
 
-	putStateErr := ctx.GetStub().PutState(LicenseId, companyScoreAssetJSON) // new state added to the tallyscore ledger
-    fmt.Printf("Asset creation returned : %s\n", putStateErr)
-    return putStateErr
+    	fmt.Printf("Creating new asset for this votable id in voting ledger: %s\n", votableId)
+	putStateErr := ctx.GetStub().PutState(votableId, votableOptionJSON) // new state added to the voting ledger
+    	return putStateErr
 
 } 
 
-// function to unregister a company (deleting it's Score asset)
-func (s *SmartContract) UnregisterCompany(ctx contractapi.TransactionContextInterface, LicenseId string) error{
-	//checking if licenseID is valid
-	// var sumOfDigits int
-	// for _, charDigit:= range LicenseId{
-	// 	digit:= int(charDigit- '0')
-	// 	sumOfDigits+= digit
-	// }
-	// if sumOfDigits%9 !=0{
-	// 	return fmt.Errorf("Invalid license ID")
-	// }
-
-	exists, err := s.companyAssetExists(ctx, LicenseId)
+// function to read vote option
+func (s *SmartContract) ReadOption(ctx contractapi.TransactionContextInterface, votableId string) (*VotableOption, error){
+	if s.Initialized != true {
+		return false,  fmt.Errorf("Ledger not inialized!")
+	}
+	votableOptionJSON, err := ctx.GetStub().GetState(votableId)
 	if err != nil {
+		return nil, fmt.Errorf("Failed to read from world state: %v", err)
+	}
+	if votableOptionJSON == nil {
+    		return nil, fmt.Errorf("The votable id %s is not present.", votableId)
+    	}
+
+	var votableOption VotableOption
+	err = json.Unmarshal(votableOptionJSON, &votableOption)
+	if err != nil {
+		return nil, err
+	}
+
+	return &votableOption, nil
+}
+
+// function to cast vote
+func (s *SmartContract) castVote(ctx contractapi.TransactionContextInterface,userId string, votableIds []string) error {
+	if s.Initialized != true {
+		return false,  fmt.Errorf("Ledger not inialized!")
+	}
+	
+	if len(votableIds) == 0 {
+		if s.IsAbstainable {
+			return castVote(str, {Abstained})
+		}
+		return fmt.Error("Number of votes to be casted can not be zero.")
+	}
+	if ( len(votableIds) > 1 && s.SingleChoice ) {
+		return fmt.Error("Number of votes can not be more than one, in case of single choice voting.")
+	}
+	//Get all current voting options
+	fmt.Println("Fetching all possible votable options ...")
+	options, err := s.GetAllOptions(ctx)
+	if err != nil {
+		fmt.Println("Could not get all voting options : %v", err)
 		return err
 	}
-	if !exists {
-		return fmt.Errorf("the asset %s does not exist", LicenseId)
+	
+	var updatedOptions []VotableOptions
+
+	for i, votableId := range options {
+		fmt.Println(i, "->", votableId) 
+		votableOption, err := s.ReadOption(ctx, votableId) // asset is read
+		if err != nil {
+			fmt.Println("Could not retrieve option %s : %v", votableId, err)
+			continue
+		}
+
+		for j, ballot := votableOption.Ballots {
+			fmt.Println("   Ballot ", j, "->", ballot.Timestamp, ";", ballot.CastedBy)
+			if ballot.CastedBy == userId {
+				return fmt.Error("User %s already casted the vote!", userId)
+			}
+		}
+		if contains(votableIds, votablId) {
+			append(votableId.Ballots, Ballot {CastedBy: s.IsAnonymous?"Anonymous":userId, Timestamp: time.Now})
+			append(updatedOptions, votableId)
+		}
 	}
-		
-	delStateOp:= ctx.GetStub().DelState(LicenseId)
-    fmt.Printf("Message received on deletion: %s", delStateOp)
-    return nil
+	for i, updated := range updatedOptions {
+		fmt.Println("Registering vote for %s : %s", userId, updated.VotableId)
+		updatedJSON, err := json.Marshal(updated)
+		if err != nil {
+			return err
+		}
+		updatestate_err := ctx.GetStub().PutState(updated.VotableId, updatedJSON)
+	}
+	return nil
+   }
 }
+// GetAllAssets returns all voting options found in world state
+func (s *SmartContract) GetAllOptions(ctx contractapi.TransactionContextInterface) ([]*VotingOption, error) {
+	// range query with empty string for startKey and endKey does an
+	// open-ended query of all assets in the chaincode namespace.
+	resultsIterator, err := ctx.GetStub().GetStateByRange("", "")
+	if err != nil {
+		return nil, err
+	}
+	defer resultsIterator.Close()
 
-// function to read companyasset
-func (s *SmartContract) ReadCompanyAsset(ctx contractapi.TransactionContextInterface, licenseID string) (*TallyScoreAsset, error){
-	companyScoreAssetJSON, err := ctx.GetStub().GetState(licenseID)
-    if err != nil {
-    	return nil, fmt.Errorf("Failed to read from world state: %v", err)
-    }
-    if companyScoreAssetJSON == nil {
-    	return nil, fmt.Errorf("The company with ID %s is not registered.", licenseID)
-    }
+	var assets []*VotingOption
+	for resultsIterator.HasNext() {
+		queryResponse, err := resultsIterator.Next()
+		if err != nil {
+			return nil, err
+		}
 
-    var companyScoreAsset TallyScoreAsset
-    err = json.Unmarshal(companyScoreAssetJSON, &companyScoreAsset)
-    if err != nil {
-    	return nil, err
+		var asset VotingOption
+		err = json.Unmarshal(queryResponse.Value, &asset)
+		if err != nil {
+			return nil, err
+		}
+		assets = append(assets, &asset)
 	}
 
-	return &companyScoreAsset, nil
-}
-
-// function to increase tallyScore of a company
-func (s *SmartContract) IncreaseScore(ctx contractapi.TransactionContextInterface, licenseID string, incrementValue string) (*TallyScoreAsset, error) {
-    companyAssetRead, err := s.ReadCompanyAsset(ctx, licenseID) // asset is read
-    if err != nil {
-    	return nil, err
-    }
-
-    intermediateUpdateval, err := strconv.ParseUint(incrementValue, 10, 32)
-    if err !=nil {
-    	fmt.Println(err)
-    }
-	newScore:= uint(companyAssetRead.Score) + ((1000- companyAssetRead.Score) * uint(intermediateUpdateval))/100
-    if newScore > 1000 {
-    	return nil, fmt.Errorf("You cannot have a value more than 1000.")
-    }
-
-    // overwriting original asset with new value
-    companyAsset := TallyScoreAsset {
-        LicenseId: licenseID,
-		Score: newScore,
-    }
-    companyAssetJSON, err := json.Marshal(companyAsset)
-    if err != nil {
-    	return nil, err
-	}
-
-	updatestate_err := ctx.GetStub().PutState(licenseID, companyAssetJSON)
-	fmt.Printf("Increasing company asset Score returned the following: %s ", updatestate_err)
-	return &companyAsset, nil
-}
-
-// function to decrease tallyScore of a company
-func (s *SmartContract) DecreaseScore(ctx contractapi.TransactionContextInterface, licenseID string, decrementValue string) (*TallyScoreAsset, error) {
-    companyAssetRead, err := s.ReadCompanyAsset(ctx, licenseID) // asset is read
-    if err != nil {
-    	return nil, err
-    }
-
-    intermediateUpdateval, err := strconv.ParseUint(decrementValue, 10, 32)
-    if err !=nil {
-    	fmt.Println(err)
-    }
-
-	updateVal:= ((1000- companyAssetRead.Score) * uint(intermediateUpdateval))/100
-	if updateVal>uint(companyAssetRead.Score){
-		return nil, fmt.Errorf("You cannot have a value lesser than 0.")
-	}
-	newScore:= uint(companyAssetRead.Score) - updateVal
-
-
-    // overwriting original asset with new value
-    companyAsset := TallyScoreAsset {
-        LicenseId: licenseID,
-		Score: newScore,
-    }
-    companyAssetJSON, err := json.Marshal(companyAsset)
-    if err != nil {
-    	return nil, err
-	}
-
-	updatestate_err := ctx.GetStub().PutState(licenseID, companyAssetJSON)
-	fmt.Printf("Decreasing company asset Score returned the following: %s ", updatestate_err)
-	return &companyAsset, nil
+	return assets, nil
 }
