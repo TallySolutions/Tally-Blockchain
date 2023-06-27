f20521aba6761de00ef035cb5fca03d07118f535
Tuhin Sengupta <tuhin.sengupts@tallysolutions.com>
2023-06-27 05:05:45 +0000

    approve voucher dialog box done

--- TallyScoreProject/website-for-user-management/src/DialogBoxes/ApproveVoucherDialogBox.js
+++ TallyScoreProject/website-for-user-management/src/DialogBoxes/ApproveVoucherDialogBox.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-function CancelVoucherDialog({ onClose }) {
+function ApproveVoucherDialog({ onClose }) {
   const [formData, setFormData] = useState({
     voucherID: '',
   });
@@ -8,7 +8,7 @@ function CancelVoucherDialog({ onClose }) {
   const [voucherDetails, setVoucherDetails] = useState('');
   const [isFormSubmitted, setIsFormSubmitted] = useState(false);
   const [showVoucherDetails, setShowVoucherDetails] = useState(false);
-  const [showCancelVoucherButton, setShowCancelVoucherButton] = useState(false);
+  const [showApproveVoucherButton, setShowApproveVoucherButton] = useState(false);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -25,19 +25,19 @@ function CancelVoucherDialog({ onClose }) {
     // CALL READVOUCHER ENDPOINT HERE
     console.log('Form submitted:', formData);
     setVoucherDetails('o/p generated from read voucher');
-    setShowCancelVoucherButton(true);
+    setShowApproveVoucherButton(true);
   };
 
   const handleButtonClick = (action) => {
-    console.log('Cancel Voucher Button Clicked:', action);
+    console.log('Approve Voucher Button Clicked:', action);
     if (action === 'Back') {
       onClose();
     }
-    if (action === 'Cancel Voucher') {
+    if (action === 'Approve Voucher') {
       if (formData.voucherID.trim() !== '') {
         onClose();
-        // REPLACE THE ABOVE LINE WITH CANCEL VOUCHER API ENDPOINT CALL
-        alert('Voucher set to cancelled.');
+        // REPLACE THE ABOVE LINE WITH APPROVE VOUCHER API ENDPOINT CALL
+        alert('Voucher set to approved.');
       } else {
         alert('Please enter a voucher ID.');
       }
@@ -45,12 +45,12 @@ function CancelVoucherDialog({ onClose }) {
   };
 
   return (
-    <div className="cancel-voucher-dialog">
+    <div className="approve-voucher-dialog">
       <button className="close-dialog-button" onClick={() => handleButtonClick('Back')}>
         Back
       </button>
       <form onSubmit={handleSubmit}>
-        <div className="cancel-voucher-form-group">
+        <div className="approve-voucher-form-group">
           <label htmlFor="voucherID">Voucher ID:</label>
           <input
             type="text"
@@ -61,7 +61,7 @@ function CancelVoucherDialog({ onClose }) {
             required
           />
         </div>
-        <div className="cancel-voucher-form-buttons">
+        <div className="approve-voucher-form-buttons">
           <button id="verify-voucher-button" type="submit">
             Verify Voucher
           </button>
@@ -72,13 +72,13 @@ function CancelVoucherDialog({ onClose }) {
           <input className="voucher-details-display" type="text" value={voucherDetails} readOnly />
         </div>
       )}
-      {showCancelVoucherButton && (
-        <div className="cancel-voucher-form-buttons">
+      {showApproveVoucherButton && (
+        <div className="approve-voucher-form-buttons">
           <button
-            className="cancel-voucher-button"
-            onClick={() => handleButtonClick('Cancel Voucher')}
+            className="approve-voucher-button"
+            onClick={() => handleButtonClick('Approve Voucher')}
           >
-            Cancel Voucher
+            Approve Voucher
           </button>
         </div>
       )}
@@ -89,4 +89,4 @@ function CancelVoucherDialog({ onClose }) {
   );
 }
 
-export default CancelVoucherDialog;
+export default ApproveVoucherDialog;
--- TallyScoreProject/website-for-user-management/src/DialogBoxes/SupplierVoucherDialog.js
+++ TallyScoreProject/website-for-user-management/src/DialogBoxes/SupplierVoucherDialog.js
@@ -1,23 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ApproveVoucherDialogBox from './ApproveVoucherDialogBox';
 
 function SupplierVoucherDialog({ onClose }) {
+  const [showApproveVoucherDialog, setShowApproveVoucherDialog] = useState(false);
+
   const handleButtonClick = (action) => {
     console.log('Supplier Voucher Button Clicked:', action);
-    onClose();
+    if (action === 'Approve Voucher') {
+      setShowApproveVoucherDialog(true);
+    } else {
+      onClose();
+    }
   };
 
   return (
-
-    <div className='voucher-dialog'>
-        <button className='close-dialog-button' onClick={() => handleButtonClick('Close Dialog')}>Back</button>  
-        <div className="voucher-dialog-buttons">
-            <button className="dialog-buttons" onClick={() => handleButtonClick('Approve Voucher')}>Approve Voucher</button>
-            <button className="dialog-buttons" onClick={() => handleButtonClick('Reject Voucher')}>Reject Voucher</button>
-            <button className="dialog-buttons" onClick={() => handleButtonClick('Send Back Voucher')}>Send Back Voucher</button>
-            <button className="dialog-buttons" onClick={() => handleButtonClick('List of Vouchers as Supplier')}>List of Vouchers as Supplier</button>
-        </div>
+    <div className="voucher-dialog">
+      {!showApproveVoucherDialog && (
+        <>
+          <button className="close-dialog-button" onClick={() => handleButtonClick('Close Dialog')}>
+            Back
+          </button>
+          <div className="voucher-dialog-buttons">
+            <button className="dialog-buttons" onClick={() => handleButtonClick('Approve Voucher')}>
+              Approve Voucher
+            </button>
+            <button className="dialog-buttons" onClick={() => handleButtonClick('Reject Voucher')}>
+              Reject Voucher
+            </button>
+            <button className="dialog-buttons" onClick={() => handleButtonClick('Send Back Voucher')}>
+              Send Back Voucher
+            </button>
+            <button className="dialog-buttons" onClick={() => handleButtonClick('List of Vouchers as Supplier')}>
+              List of Vouchers as Supplier
+            </button>
+          </div>
+        </>
+      )}
+      {showApproveVoucherDialog && (
+        <ApproveVoucherDialogBox onClose={() => setShowApproveVoucherDialog(false)} />
+      )}
     </div>
-
   );
 }
 
